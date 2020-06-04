db2c40ded6fe1bb752394dda34e9c217ebfd4a79
Byron Katz <byron.katz@coveros.com>
2020-06-04 11:35:53 -0400

    Playing around
    Trying out the syntax of Kotlin, playing with TDD and pair programming.  Warning: one test is broken, two are unimplemented.

--- src/main/kotlin/r3z/App.kt
+++ src/main/kotlin/r3z/App.kt
@@ -1,29 +1,61 @@
+
 package r3z
 
 class App {
+
     val greeting: String
         get() {
             return "Hello world."
         }
-
-    fun thing() : String {
-        fun inner1() : String {
-            fun inner2() : String {
-                fun inner3() : String {
-                    fun inner4() : String {
-                        return "FOO!!!"
-                    }
-                    return inner4()
-                }
-                return inner3()
-            }
-            return inner2()
-        }
-        return inner1()
-    }
 }
 
 fun main(args: Array<String>) {
     println(App().greeting)
-    println(App().thing())
+}
+
+fun mattBaddassAdder(num1 : Int, num2 : Int) : Int {
+    val result = num1 + num2
+    return result
+}
+
+fun byronBaddassSinglePurposePow(num : Int) : Int {
+    return num * num
+}
+
+fun mockMeBaby(input : String) : String {
+    var result = ""
+    var tracker = false
+    for(letter in input) {
+        if (letter == ' ') {
+            result += ' '
+            continue
+        }
+        if (tracker) {
+            result += letter.toUpperCase()
+            tracker = false
+        } else {
+            result += letter.toLowerCase()
+            tracker = true
+        }
+    }
+    return result
+}
+
+/**
+ * Takes naughty potty-mouth text and takes that shit out
+ */
+fun restrictMySpeech(text: String) : String {
+    val pottywords = listOf("fuck", "shit")
+    var regexes : MutableList<String> = mutableListOf()
+    for (word in pottywords) {
+        var newword = ""
+        for (letter in word) {
+            newword += "$letter.?"
+        }
+        newword += "[^ ]* ?"
+        regexes.add(newword)
+    }
+    val regex = Regex(regexes.joinToString("|"), RegexOption.IGNORE_CASE)
+    // val regex = Regex("f.?u.?c.?k.?[^ ]* ?|shit[^ ]* ?")
+    return regex.replace(text, "").trim()
 }
