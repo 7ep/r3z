5799aa6d8f678cfdf025f77a8d098209d2aeb8d7
Byron Katz <byron.katz@coveros.com>
2020-06-04 11:35:53 -0400

    Playing around
    Trying out the syntax of Kotlin, playing with TDD and pair programming.  Warning: one test is broken, two are unimplemented.
    
    Smarter search for bad words
    
    Now our code will look for words that use separator characters, like "."
    
    Co-authored-by: Byron Katz <byron.katz@coveros.com>
    Co-authored-by: Matt Taylor <matthew.taylor@coveros.com>

--- src/main/kotlin/r3z/App.kt
+++ src/main/kotlin/r3z/App.kt
@@ -1,29 +1,58 @@
+
 package r3z
 
 class App {
+
     val greeting: String
         get() {
             return "Hello world."
         }
-
-    fun thing() : String {
-        fun inner1() : String {
-            fun inner2() : String {
-                fun inner3() : String {
-                    fun inner4() : String {
-                        return "FOO!!!"
-                    }
-                    return inner4()
-                }
-                return inner3()
-            }
-            return inner2()
-        }
-        return inner1()
-    }
 }
 
 fun main(args: Array<String>) {
     println(App().greeting)
-    println(App().thing())
+}
+
+fun mattBaddassAdder(num1 : Int, num2 : Int) : Int {
+    val result = num1 + num2
+    return result
+}
+
+fun byronBaddassSinglePurposePow(num : Int) : Int {
+    return num * num
+}
+
+fun mockMeBaby(input : String) : String {
+    var result = ""
+    var tracker = false
+    for(letter in input) {
+        if (letter == ' ') {
+            result += ' '
+            continue
+        }
+        if (tracker) {
+            result += letter.toUpperCase()
+            tracker = false
+        } else {
+            result += letter.toLowerCase()
+            tracker = true
+        }
+    }
+    return result
+}
+
+/**
+ * Takes naughty potty-mouth text and takes that shit out
+ */
+fun restrictMySpeech(text: String) : String {
+    val bannedWords = listOf("fuck", "shit")
+    val regexes : MutableList<String> = mutableListOf()
+    for (word in bannedWords) {
+        val partialRegex = word.toCharArray()
+                .joinToString(".?") +  // I want to potentially handle a single-char divider between letters
+                "[^ ]* ?"              // zero or more "not-a-space", ending at a space
+        regexes.add(partialRegex)
+    }
+    val regex = Regex(regexes.joinToString("|"), RegexOption.IGNORE_CASE)
+    return regex.replace(text, "").trim()
 }
