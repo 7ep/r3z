8b0c17407eb4bf001b25d90871b947f7b23eb1aa
Byron Katz <byron.katz@coveros.com>
2020-06-19 09:02:30 -0400

    Data class FTW!!!!
    Co-authored-by: Byron Katz <byron.katz@coveros.com>
    Co-authored-by: Matt Taylor <matthew.taylor@coveros.com>

--- src/main/kotlin/com/coveros/r3z/domainobjects/TimeEntry.kt
+++ src/main/kotlin/com/coveros/r3z/domainobjects/TimeEntry.kt
@@ -1,18 +1,3 @@
 package com.coveros.r3z.domainobjects
 
-class TimeEntry(val user: User, val project: Project, val time: Time, val details : Details = Details("")) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return javaClass.hashCode()
-    }
-
-    override fun toString(): String {
-        return "TimeEntry(user=$user, project=$project, time=$time, details=$details)"
-    }
-}
+data class TimeEntry(val user: User, val project: Project, val time: Time, val details : Details = Details(""))
