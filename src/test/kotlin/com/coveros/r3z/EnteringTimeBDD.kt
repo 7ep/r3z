b94de0b6efa37ee38a1f88632a8036adf08de7ad
Byron Katz <byron.katz@coveros.com>
2020-07-02 12:19:05 -0400

    Adding a performance test

--- src/test/kotlin/com/coveros/r3z/EnteringTimeBDD.kt
+++ src/test/kotlin/com/coveros/r3z/EnteringTimeBDD.kt
@@ -8,34 +8,35 @@ import com.coveros.r3z.persistence.microorm.FlywayHelper
 import com.coveros.r3z.persistence.microorm.IDbAccessHelper
 import com.coveros.r3z.timerecording.TimeEntryPersistence
 import com.coveros.r3z.timerecording.TimeRecordingUtilities
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertThrows
-import org.junit.Ignore
+import org.junit.Assert.*
 import org.junit.Test
-import java.lang.AssertionError
 
 /**
- * As an employee
- * I want to record my time
- * So that I am easily able to document my time in an organized way
+ * Feature: Entering time
+ *
+ * User story:
+ *      As an employee
+ *      I want to record my time
+ *      So that I am easily able to document my time in an organized way
  */
 class EnteringTimeBDD {
+
     /**
      * Just a happy path for entering a time entry
      */
     @Test
     fun `capability to enter time`() {
-        // `given I have worked 1 hour on project "A" on Monday`()
+        // given I have worked 1 hour on project "A" on Monday
         val expectedStatus = RecordTimeResult(1, StatusEnum.SUCCESS)
         val dbAccessHelper = initializeDatabaseForTest()
         val tru = createTimeRecordingUtility(dbAccessHelper)
         val newProject : Project = tru.createProject(ProjectName("A"))
         val entry = createTimeEntry(project = newProject)
 
-        // `when I enter in that time`()
+        // when I enter in that time
         val recordStatus = tru.recordTime(entry)
 
-        // `then the system indicates it has persisted the new information`()
+        // then the system indicates it has persisted the new information
         assertEquals("the system indicates it has persisted the new information", expectedStatus, recordStatus)
     }
 
@@ -44,7 +45,7 @@ class EnteringTimeBDD {
      */
     @Test
     fun `A user enters six hours on a project with copious notes`() {
-        // `given I have worked 6 hour on project "A" on Monday with a lot of notes`()
+        // given I have worked 6 hour on project "A" on Monday with a lot of notes
         val dbAccessHelper = initializeDatabaseForTest()
         val tru = createTimeRecordingUtility(dbAccessHelper)
         val newProject : Project = tru.createProject(ProjectName("A"))
@@ -55,10 +56,10 @@ class EnteringTimeBDD {
         )
         val expectedStatus = RecordTimeResult(1, StatusEnum.SUCCESS)
 
-        // `when I enter in that time`()
+        // when I enter in that time
         val recordStatus = tru.recordTime(entry)
 
-        // `then the system indicates it has persisted the new information`()
+        // then the system indicates it has persisted the new information
         assertEquals("the system indicates it has persisted the new information", expectedStatus, recordStatus)
     }
 
@@ -85,6 +86,41 @@ class EnteringTimeBDD {
         // the system disallows it
         val ex = assertThrows(AssertionError::class.java) { createTimeEntry(time = Time((60 * 24) + 1)) }
         assertEquals(ex.message, "Entries do not span multiple days, thus must be <=24 hrs")
+    }
+
+    @Test
+    fun `Performance test`() {
+        // given I have worked on a project
+        val dbAccessHelper = initializeDatabaseForTest()
+        val startAfterDatabase = System.currentTimeMillis()
+        val tru = createTimeRecordingUtility(dbAccessHelper)
+        val newProject : Project = tru.createProject(ProjectName("A"))
+
+        // when I enter in that time
+        val numberOfSamples = 60 * 8
+        val durations = LongArray(numberOfSamples)
+        for (i in 1..numberOfSamples) {
+            val start = System.currentTimeMillis()
+            val entry = createTimeEntry(
+                    time = Time(1),
+                    project = newProject,
+                    details = Details("Four score and seven years ago, blah blah blah"))
+            tru.recordTime(entry)
+            val finish = System.currentTimeMillis()
+            val timeElapsed = finish - start
+            durations[i-1] = timeElapsed
+        }
+
+        // the system should perform quickly
+        val average = durations.average()
+        val takesLessThanMillisecondsAverage = average < 3
+        println("Durations:")
+        durations.forEach { d -> print(" $d") }
+        val endOfTest = System.currentTimeMillis()
+        val totalTime = endOfTest - startAfterDatabase
+        println("\nThe functions took a total of $totalTime milliseconds for $numberOfSamples database calls")
+
+        assertTrue("the system should respond quickly, but with $numberOfSamples samples, this took $average", takesLessThanMillisecondsAverage)
     }
 
     /**
