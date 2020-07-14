b62b2c7faf94f15d4ee0244d45585c84f3710374
Byron Katz <byron.katz@coveros.com>
2020-07-14 10:55:56 -0400

    Adding a "TimeEntryForDatabase" class
    Instead of persisting a user with name and id, and a project with name and id, now, we just persist the identifiers.  Created new types so this is all strongly-typed.

--- src/test/kotlin/coverosR3z/persistence/FakeTimeEntryPersistence.kt
+++ src/test/kotlin/coverosR3z/persistence/FakeTimeEntryPersistence.kt
@@ -12,7 +12,7 @@ class FakeTimeEntryPersistence(
         val persistNewProjectBehavior : () -> Project = { DEFAULT_PROJECT }) : ITimeEntryPersistence {
 
 
-    override fun persistNewTimeEntry(entry: TimeEntry) {
+    override fun persistNewTimeEntry(entry: TimeEntryForDatabase) {
         persistNewTimeEntryBehavior()
     }
 
@@ -29,7 +29,7 @@ class FakeTimeEntryPersistence(
         return minutesRecorded
     }
 
-    override fun readTimeEntries(user: User): List<TimeEntry>? {
+    override fun readTimeEntries(user: User): List<TimeEntryForDatabase>? {
         return listOf()
     }
 
