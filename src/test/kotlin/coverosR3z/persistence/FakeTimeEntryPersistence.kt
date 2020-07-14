887908d6f029484b2307f82ea23dcf95fc90a1f3
Byron Katz <byron.katz@coveros.com>
2020-07-14 09:22:00 -0400

    Refactoring and new asserts

---
+++ src/test/kotlin/coverosR3z/persistence/FakeTimeEntryPersistence.kt
@@ -0,0 +1,36 @@
+package coverosR3z.persistence
+
+import coverosR3z.DEFAULT_PROJECT
+import coverosR3z.DEFAULT_USER
+import coverosR3z.domainobjects.*
+import coverosR3z.timerecording.ITimeEntryPersistence
+
+
+class FakeTimeEntryPersistence(
+        val minutesRecorded: Long = 0L,
+        val persistNewTimeEntryBehavior : () -> Unit = {},
+        val persistNewProjectBehavior : () -> Project = { DEFAULT_PROJECT }) : ITimeEntryPersistence {
+
+
+    override fun persistNewTimeEntry(entry: TimeEntry) {
+        persistNewTimeEntryBehavior()
+    }
+
+    override fun persistNewProject(projectName: ProjectName): Project {
+        return persistNewProjectBehavior()
+    }
+
+    override fun persistNewUser(username: UserName): User {
+        return DEFAULT_USER
+    }
+
+
+    override fun queryMinutesRecorded(user: User, date: Date): Long {
+        return minutesRecorded
+    }
+
+    override fun readTimeEntries(user: User): List<TimeEntry>? {
+        return listOf()
+    }
+
+}
