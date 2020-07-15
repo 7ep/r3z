8443f6981d359af30233efc914f343ac0891b6b5
Byron Katz <byron.katz@coveros.com>
2020-07-14 23:44:11 -0400

    Adding tests for PureMemoryDatabase.kt

--- src/test/kotlin/coverosR3z/TestHelpers.kt
+++ src/test/kotlin/coverosR3z/TestHelpers.kt
@@ -9,8 +9,10 @@ val A_RANDOM_DAY_IN_JUNE_2020 = Date(2020, Month.JUN, 25)
 val A_RANDOM_DAY_IN_JUNE_2020_PLUS_ONE = Date(2020, Month.JUN, 26)
 val THREE_HOURS_FIFTEEN = Time((3 * 60) + 15)
 val DEFAULT_USER = User(1, "I")
+val DEFAULT_USERNAME = UserName("I")
 val DEFAULT_TIME = Time(60)
 val DEFAULT_PROJECT = Project(1, "A")
+val DEFAULT_PROJECTNAME = ProjectName("A")
 
 fun createTimeEntry(
         id : Int = 1,
