fc26832fa65d1abbee0cbc8dd8786053887f3e22
Byron Katz <byron.katz@coveros.com>
2020-07-15 16:14:30 -0400

    Refactoring, starting a new test idea

--- src/test/kotlin/coverosR3z/persistence/PureMemoryDatabaseTests.kt
+++ src/test/kotlin/coverosR3z/persistence/PureMemoryDatabaseTests.kt
@@ -17,29 +17,53 @@ class PureMemoryDatabaseTests {
 
     @Test fun `should be able to add a new project`() {
         pmd.addNewProject(DEFAULT_PROJECT_NAME)
+
         val project = pmd.getProjectById(DEFAULT_PROJECT.id)
+
         assertEquals(1, project!!.id)
     }
 
     @Test fun `should be able to add a new user`() {
         pmd.addNewUser(DEFAULT_USERNAME)
+
         val user = pmd.getUserById(DEFAULT_USER.id)
+
         assertEquals(1, user!!.id)
     }
 
     @Test fun `should be able to add a new time entry`() {
         pmd.addTimeEntry(TimeEntryPreDatabase(DEFAULT_USER, DEFAULT_PROJECT, DEFAULT_TIME, A_RANDOM_DAY_IN_JUNE_2020))
+
         val timeEntries = pmd.getAllTimeEntriesForUser(DEFAULT_USER)[0]
-        assertEquals(1, timeEntries.id)
+
+        assertEquals(1, timeEntries.id)N
         assertEquals(DEFAULT_USER, timeEntries.user)
         assertEquals(DEFAULT_PROJECT, timeEntries.project)
         assertEquals(DEFAULT_TIME, timeEntries.time)
         assertEquals(A_RANDOM_DAY_IN_JUNE_2020, timeEntries.date)
     }
 
+    @Test fun `a 200-person firm should be able to add time entries for 10 years`() {
+        // generate the 200 users
+
+        // generate 2000 projects
+
+        // add time entries for those users,
+        // adding 4 entries per day and random text between 0 and 500 chars in details
+        // for 5 days a week, for 10 years
+
+//        pmd.addTimeEntry(Time)
+
+        // we should be able to get the entries for years ago on a given day right fast
+
+        // we should be able to run a cumulative report on data for 10 years right fast on a given user
+    }
+
     @Test fun `should be able to get the minutes on a certain date`() {
         pmd.addTimeEntry(TimeEntryPreDatabase(DEFAULT_USER, DEFAULT_PROJECT, DEFAULT_TIME, A_RANDOM_DAY_IN_JUNE_2020))
+
         val minutes = pmd.getMinutesRecordedOnDate(DEFAULT_USER, A_RANDOM_DAY_IN_JUNE_2020)
+
         assertEquals(DEFAULT_TIME.numberOfMinutes, minutes)
     }
 
