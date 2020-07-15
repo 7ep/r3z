8c930b3c93f8e51eaa60d457ea34e3a7372a3f8a
Byron Katz <byron.katz@coveros.com>
2020-07-14 23:44:11 -0400

    Adding tests for PureMemoryDatabase.kt

--- src/test/kotlin/coverosR3z/persistence/PureMemoryDatabaseTests.kt
+++ src/test/kotlin/coverosR3z/persistence/PureMemoryDatabaseTests.kt
@@ -1,8 +1,12 @@
 package coverosR3z.persistence
 
+import coverosR3z.*
 import coverosR3z.domainobjects.*
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
+import kotlin.time.minutes
 
 class PureMemoryDatabaseTests {
 
@@ -20,36 +24,37 @@ class PureMemoryDatabaseTests {
         assertTrue(tes[1].project === project)
     }
 
-//    @Before
-//    fun init() {
-//        pmd = PureMemoryDatabase()
-//    }
-//
-//    @Test fun `should be able to add a new project`() {
-//        pmd.addNewProject(ProjectName("my project"))
-//    }
-//
-//    @Test fun `should be able to add a new user`() {
-//        pmd.addNewUser(UserName("some user"))
-//    }
-//
-//    @Test fun `should be able to add a new time entry`() {
-//        pmd.addTimeEntry(TimeEntryForDatabase())
-//    }
-//
-//    @Test fun `should be able to get the minutes on a certain date`() {
-//        pmd.getMinutesRecordedOnDate()
-//    }
-//
-//    @Test fun `should be able to get all time entries for a user`() {
-//        pmd.getAllTimeEntriesForUser()
-//    }
-//
-//    @Test fun `should be able to get a project by id`() {
-//        pmd.getProjectById()
-//    }
-//
-//    @Test fun `should be able to get a user by id`() {
-//        pmd.getUserById()
-//    }
+    @Before
+    fun init() {
+        pmd = PureMemoryDatabase()
+    }
+
+    @Test fun `should be able to add a new project`() {
+        pmd.addNewProject(DEFAULT_PROJECTNAME)
+        val project = pmd.getProjectById(DEFAULT_PROJECT.id)
+        assertEquals(1, project!!.id)
+    }
+
+    @Test fun `should be able to add a new user`() {
+        pmd.addNewUser(DEFAULT_USERNAME)
+        val user = pmd.getUserById(DEFAULT_USER.id)
+        assertEquals(1, user!!.id)
+    }
+
+    @Test fun `should be able to add a new time entry`() {
+        pmd.addTimeEntry(TimeEntry(1, DEFAULT_USER, DEFAULT_PROJECT, DEFAULT_TIME, A_RANDOM_DAY_IN_JUNE_2020))
+        val timeEntries = pmd.getAllTimeEntriesForUser(DEFAULT_USER)[0]
+        assertEquals(1, timeEntries.id)
+        assertEquals(DEFAULT_USER, timeEntries.user)
+        assertEquals(DEFAULT_PROJECT, timeEntries.project)
+        assertEquals(DEFAULT_TIME, timeEntries.time)
+        assertEquals(A_RANDOM_DAY_IN_JUNE_2020, timeEntries.date)
+    }
+
+    @Test fun `should be able to get the minutes on a certain date`() {
+        pmd.addTimeEntry(TimeEntry(1, DEFAULT_USER, DEFAULT_PROJECT, DEFAULT_TIME, A_RANDOM_DAY_IN_JUNE_2020))
+        val minutes = pmd.getMinutesRecordedOnDate(DEFAULT_USER, A_RANDOM_DAY_IN_JUNE_2020)
+        assertEquals(DEFAULT_TIME.numberOfMinutes, minutes)
+    }
+
 }
