e0d6f107b311f011d86050da2becde1c64110fe1
Byron Katz <byron.katz@coveros.com>
2020-07-18 23:40:05 -0400

    Testing User serialization

--- src/main/kotlin/coverosR3z/domainobjects/User.kt
+++ src/main/kotlin/coverosR3z/domainobjects/User.kt
@@ -1,23 +1,25 @@
 package coverosR3z.domainobjects
 
+import coverosR3z.exceptions.MalformedDataDuringSerializationException
 import java.lang.Integer.parseInt
 
 private const val maxEmployeeCount = 100_000_000
 private const val maxEmployeeMsg = "No way this company has more than 100 million employees"
+private const val nameCannotBeEmptyMsg = "All users must have a non-empty name"
 
 /**
  * Holds a user's name before we have a whole object, like [User]
  */
 data class UserName(val value: String) {
     init {
-        assert(value.isNotEmpty()) {"All users must have a non-empty name"}
+        assert(value.isNotEmpty()) {nameCannotBeEmptyMsg}
     }
 }
 
 data class User(val id: Int, val name: String) {
 
     init {
-        assert(name.isNotEmpty()) {"All users must have a non-empty name"}
+        assert(name.isNotEmpty()) {nameCannotBeEmptyMsg}
         assert(id < maxEmployeeCount) { maxEmployeeMsg }
     }
 
@@ -29,14 +31,15 @@ data class User(val id: Int, val name: String) {
         private val deserializationRegex = "\\{id=(.*),name=(.*)}".toRegex()
 
         fun deserialize(value : String) : User? {
-            val matches = deserializationRegex.matchEntire(value)
-            if (matches != null) {
+            try {
+                val matches = deserializationRegex.matchEntire(value) ?: throw Exception()
                 val (idString, name) = matches.destructured
                 val id = parseInt(idString)
                 return User(id, name)
-            } else {
-                return null
+            } catch (ex : Exception) {
+                throw MalformedDataDuringSerializationException("was unable to deserialize this: ( $value )")
             }
+
         }
     }
 
