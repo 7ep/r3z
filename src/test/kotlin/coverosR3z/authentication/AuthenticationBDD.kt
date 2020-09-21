69fcbff43bf3f0d35f5b4f6148a13b46abd45d09
Matthew <matthew.taylor@coveros.com>
2020-09-21 16:50:36 -0400

    Use real names, not defaults, in BDD

--- src/test/kotlin/coverosR3z/authentication/AuthenticationBDD.kt
+++ src/test/kotlin/coverosR3z/authentication/AuthenticationBDD.kt
@@ -18,31 +18,32 @@ import coverosR3z.domainobjects.LoginStatuses as ls
  */
 class AuthenticationBDD {
 
-    val cua = CurrentUserAccessor() // since we have a method to clear this, we can share it between tests
+    private val cua = CurrentUserAccessor() // since we have a method to clear this, we can share it between tests
+
     /**
      * The key here is that when we do this, it's
      * recording who does what, and only allows proper
-     * people to carry out actions.  Like, Alice
-     * can only modify Alice's stuff.
+     * people to carry out actions.
      */
-
-    fun `set up authentication and employee`() : Pair<TimeRecordingUtilities, Employee>{
-        cua.clearCurrentUserTestOnly() // We need to use cua for this test, sharing a pmd with auth persistence
+    fun loginAsAliceCreateEmployeesAliceAndSarah() : Pair<TimeRecordingUtilities, Employee>{
+        // We need to use cua for this test, sharing a pmd with auth persistence
         // and time recording persistence, in order to avoid recordTime throwing a USER_EMPLOYEE_MISMATCH status
+        cua.clearCurrentUserTestOnly()
         val pmd = PureMemoryDatabase()
         val authPersistence = AuthenticationPersistence(pmd)
         val au = AuthenticationUtilities(authPersistence)
-        au.register(DEFAULT_USER.name, DEFAULT_PASSWORD, 2) // we know DEFAULT_EMPLOYEE has an id=1
-        au.login(DEFAULT_USER.name, DEFAULT_PASSWORD)
+
+        au.register("alice", DEFAULT_PASSWORD, 1) // we know DEFAULT_EMPLOYEE has an id=1
+        au.login("alice", DEFAULT_PASSWORD)
 
         // Perform some quick checks
-        val user = authPersistence.getUser(UserName(DEFAULT_USER.name))
-
-        assertEquals(user, cua.get()) // auth persistence and user persistence must agree
-        assertTrue("our user should be registered", au.isUserRegistered(DEFAULT_USER.name)) // registration must succeed
+        assertEquals("Auth persistence and user persistence must agree",
+                authPersistence.getUser(UserName("alice")), cua.get())
+        assertTrue("Registration must have succeeded", au.isUserRegistered("alice"))
 
         val tru = TimeRecordingUtilities(TimeEntryPersistence(pmd))
-        val sarah = tru.createEmployee(EmployeeName("Sarah"))
+        tru.createEmployee(EmployeeName("Alice")) // Since employee ids are assigned on creation, Alice will have id=1
+        val sarah = tru.createEmployee(EmployeeName("Sarah")) // and Sarah will have id=2
         tru.createProject(DEFAULT_PROJECT_NAME)
 
         return Pair(tru, sarah)
@@ -72,27 +73,27 @@ class AuthenticationBDD {
 
     @Test
     fun `I can add a time entry`() {
-        // given I am logged in
+        // Given I am logged in
         val tru = `setup basic authentication system`()
 
-        // when I add a time entry
+        // When I add a time entry
         val entry = createTimeEntryPreDatabase(DEFAULT_EMPLOYEE)
         val result = tru.recordTime(entry)
 
-        // then it proceeds successfully
+        // Then it proceeds successfully
         assertEquals(RecordTimeResult(StatusEnum.SUCCESS), result)
     }
 
     @Test
     fun `I cannot change someone else's time`() {
-        //given I am logged in as "default" and employee Sarah exists in the database
-        val (tru, sarah) = `set up authentication and employee`()
+        // Given I am logged in as user "alice" and employees Sarah and Alice exist in the database
+        val (tru, sarah) = loginAsAliceCreateEmployeesAliceAndSarah()
 
-        // when I try to add a time-entry for Sarah
+        // When I try to add a time-entry for Sarah
         val entry = createTimeEntryPreDatabase(sarah)
         val result = tru.recordTime(entry)
 
-        // then the system disallows it
+        // Then the system disallows it
         assertEquals(RecordTimeResult(StatusEnum.USER_EMPLOYEE_MISMATCH), result)
     }
 
