674b2d3fbefd215b113f2b1bd16049d2d638e6e5
Matthew <matthew.taylor@coveros.com>
2020-10-01 11:03:24 -0400

    Add failing test for templating

---
+++ src/test/kotlin/coverosR3z/templating/TemplatingTests.kt
@@ -0,0 +1,22 @@
+package coverosR3z.templating
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import coverosR3z.templating.*
+
+class TemplatingTests {
+
+    val te = TemplatingEngine()
+
+    @Test
+    fun `should be able to generate html with a variable substituted`() {
+        var expected = "<body>hello matt</body>"
+
+        
+        var pre = "<body>hello {{value}}</body>"
+        var actual = te.render(pre)
+
+        assertEquals(expected, actual)
+    }
+}
