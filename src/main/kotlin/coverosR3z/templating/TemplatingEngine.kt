7d4fed2132fd1896efc2fb3903d27a4dfbf0d979
Matthew <matthew.taylor@coveros.com>
2020-10-01 12:21:27 -0400

    Add basic substitution templating capabilities

--- src/main/kotlin/coverosR3z/templating/TemplatingEngine.kt
+++ src/main/kotlin/coverosR3z/templating/TemplatingEngine.kt
@@ -1,10 +1,44 @@
 package coverosR3z.templating
 
+import kotlin.collections.*
+
+
 class TemplatingEngine() {
 
-    fun render(input: String): String {
-        val match = input.replace("{{value}}" , "matt")
-        return match
+    fun render(input: String, mapping: Map<String, String>): String {
+        var rendered = input
+        var flag = 0
+        var words = mutableListOf<String>()
+        var word = ""
+        for (s in input) {
+            println(s)
+            if (flag == 2 && s != '}') {
+                word += s
+            }
+            if (s == '{') {
+                flag++
+            }
+            if (s == '}') {
+                flag = 0
+                println(word)
+                words.add(word)
+                word = ""
+            }
+        }
+
+        println("Values in words are: ")
+        println(words)
+
+        for (word in words) {
+            if (word in mapping.keys) {
+                println("replacing instances of '$word' with '${mapping[word]}'")
+                rendered = rendered.replace("{{$word}}", mapping[word]?: "")
+                println(rendered)
+            }
+
+        }
+
+        return rendered
     }
 
 }
--- src/test/kotlin/coverosR3z/templating/TemplatingTests.kt
+++ src/test/kotlin/coverosR3z/templating/TemplatingTests.kt
@@ -12,9 +12,25 @@ class TemplatingTests {
     @Test
     fun `should be able to generate html with a variable substituted`() {
         var expected = "<body>hello matt</body>"
-        var pre = "<body>hello {{value}}</body>"
-        var actual = te.render(pre)
+        var pre = "<body>hello {{name}}</body>"
+        var actual = te.render(pre, mapOf<String, String>("name" to "matt"))
 
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should convert a template and list of keys and values into expected html`() {
+        var actual = "<body>the {{adjective1}} {{adjective2}} {{noun1}} {{verb}} over the {{adjective3}} {{noun2}}</body>"
+        var expected = "<body>the quick brown fox jumped over the lazy dog</body>"
+
+        val mappings = mapOf<String, String>(
+            "adjective1" to "quick",
+            "adjective2" to "brown",
+            "noun1" to "fox",
+            "adjective3" to "lazy",
+            "noun2" to "dog",
+            "verb" to "jumped"
+        )
+        assertEquals(expected, te.render(actual, mappings))
     }
 }
