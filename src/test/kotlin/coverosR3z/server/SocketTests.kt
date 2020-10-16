ea6e31e1e53f999adcc769375130716e402d18eb
Matthew <matthew.taylor@coveros.com>
2020-10-16 18:02:02 -0400

    Add additional socket tests

--- src/test/kotlin/coverosR3z/server/SocketTests.kt
+++ src/test/kotlin/coverosR3z/server/SocketTests.kt
@@ -1,5 +1,6 @@
 package coverosR3z.server
 
+import coverosR3z.FileReader
 import org.junit.*
 import org.junit.Assert.assertEquals
 import java.io.BufferedReader
@@ -14,25 +15,43 @@ var serverSocket : Socket = Socket("localhost", 12321)
 class ServerSocketInitializer(): Runnable {
 
     override fun run() {
-        val halfOpenServerSocket = ServerSocket(12321)
+        var halfOpenServerSocket = ServerSocket(12321)
         serverSocket = halfOpenServerSocket.accept()
     }
 }
 
 class SocketTests() {
 
-    @Before
-    fun openSockets() {
-        val server = Thread(ServerSocketInitializer())
-        server.start()
-        clientSocket = Socket("localhost", 12321)
-    }
+    companion object{
+        @BeforeClass @JvmStatic
+        fun openSockets() {
+            val server = Thread(ServerSocketInitializer())
+            server.start()
+            clientSocket = Socket("localhost", 12321)
+        }
 
-    @After
-    fun closeSockets() {
-        serverSocket.close()
-        clientSocket.close()
+        @AfterClass @JvmStatic
+        fun closeSockets() {
+            serverSocket.close()
+            clientSocket.close()
+        }
     }
+//    @Before
+//    fun openSockets() {
+//        val server = Thread(ServerSocketInitializer())
+//        server.start()
+//        clientSocket = Socket("localhost", 12321)
+//    }
+//
+//    @After
+//    fun closeSockets() {
+//        serverSocket.shutdownInput()
+//        clientSocket.shutdownOutput()
+//        serverSocket.close()
+//        clientSocket.close()
+////        clientSocket = Socket("localhost", 12321)
+////        serverSocket = Socket("localhost", 12321)
+//    }
 
     @Test
     fun testSimpleConversation() {
@@ -67,10 +86,40 @@ class SocketTests() {
         val clientWriter = clientSocket.getOutputStream()
         val serverWriter = serverSocket.getOutputStream()
 
-        clientWriter.write("I want a homepage and I want it now\n".toByteArray())
+        clientWriter.write("GET / HTTP/1.1\n".toByteArray())
+        val serverInput = serverReader.readLine()
+        if(serverInput == "GET / HTTP/1.1"){
+            serverWriter.write("HTTP/1.1 200 OK\n <!DOCTYPE html>\n (the whole text of the page)\n".toByteArray())
+        }
 
+        clientReader.readLine()
         val response = clientReader.readLine()
 
-        assert(response.contains("Welcome to the HOME PAGE"))
+        assert(response.contains("<!DOCTYPE html>"))
+    }
+
+    @Test
+    fun `should see a welcome message from an html file when GET requesting server`() {
+        val clientReader = BufferedReader(InputStreamReader(clientSocket.inputStream))
+        val serverReader = BufferedReader(InputStreamReader(serverSocket.inputStream))
+        val clientWriter = clientSocket.getOutputStream()
+        val serverWriter = serverSocket.getOutputStream()
+
+        val webpage = FileReader.read("sample.html")
+        clientWriter.write("GET / HTTP/1.1\n".toByteArray())
+        val serverInput = serverReader.readLine()
+        if(serverInput == "GET / HTTP/1.1"){
+            serverWriter.write("HTTP/1.1 200 OK\n <!DOCTYPE html>\n${webpage}\n".toByteArray())
+        }
+
+        repeat(2) {clientReader.readLine()}
+        var body = ""
+        var nextLine = clientReader.readLine()
+        while (nextLine != "</html>") {
+            body += "${nextLine}\n"
+            nextLine = clientReader.readLine()
+        }
+        body += nextLine
+        assertEquals(webpage, body)
     }
 }
