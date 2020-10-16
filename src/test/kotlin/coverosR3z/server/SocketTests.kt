6e4ecef0ca094482797109f0bcab7cfdb4a34a77
Byron Katz <byron.katz@coveros.com>
2020-10-16 14:06:15 -0400

    Simplifying the socket tests
    Co-authored-by: Mitch Hardee <mitchell.hardee@coveros.com>
    Co-authored-by: Byron Katz <byron.katz@coveros.com>
    Co-authored-by: Matt Taylor <matthew.taylor@coveros.com>

--- src/test/kotlin/coverosR3z/server/SocketTests.kt
+++ src/test/kotlin/coverosR3z/server/SocketTests.kt
@@ -1,48 +1,48 @@
 package coverosR3z.server
 
 import coverosR3z.logging.logInfo
+import org.junit.*
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
-import org.junit.Test
 import java.io.BufferedReader
+import java.io.InputStream
 import java.io.InputStreamReader
+import java.io.OutputStream
 import java.net.ServerSocket
 import java.net.Socket
+
+
+var clientSocket : Socket = Socket("localhost", 12321)
+var serverSocket : Socket = Socket("localhost", 12321)
 
 class HTTPHandler(): Runnable {
 
     override fun run() {
-        val serverSocket = ServerSocket(12321)
-        val clientSocket = serverSocket.accept()
-        clientSocket.use {
-            val br = BufferedReader(InputStreamReader(it.inputStream))
-            println("Server received: ${br.readLine()}")
-            it.outputStream.write("HTTP/1.1 200 OK".toByteArray())
-        }
-        serverSocket.close()
+        val halfOpenServerSocket = ServerSocket(12321)
+        serverSocket = halfOpenServerSocket.accept()
     }
 }
 
 class SocketTests() {
 
-    /**
-     * If we talk to an HTTP server with the proper protocol,
-     * it should reply politely
-     */
-    @Test
-    fun testTryWithHttp() {
-        val request = "GET / HTTP/1.1\n"
-        val expectedResponse = "HTTP/1.1 200 OK"
-
+    @Before
+    fun openSockets() {
         val server = Thread(HTTPHandler())
         server.start()
+        clientSocket = Socket("localhost", 12321)
+    }
 
-        val sock = Socket("localhost", 12321)
-        sock.use {
-            it.outputStream.write(request.toByteArray())
-            val br = BufferedReader(InputStreamReader(it.inputStream))
-            val response = br.readLine()
-            println("Client received: $response")
-            assertTrue("Status message should be good", response.contains(expectedResponse))
-        }
+    @After
+    fun closeSockets() {
+        serverSocket.close()
+        clientSocket.close()
+    }
+
+    @Test
+    fun testSimpleConversation() {
+        clientSocket.getOutputStream().write("Hello\n".toByteArray())
+        val br = BufferedReader(InputStreamReader(serverSocket.inputStream))
+        val value = br.readLine()
+        assertEquals(value, "Hello")
     }
 }
