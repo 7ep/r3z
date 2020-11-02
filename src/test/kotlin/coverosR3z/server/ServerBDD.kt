36232430ce6417ee0ebc1076edc30c4be51eeee9
Byron Katz <byron.katz@coveros.com>
2020-11-01 21:03:32 -0500

    kind-of working BDD file for the server.
    Upgrading version of Kotlin, including dependencies for Selenium Chromedriver and HtmlUnit

--- src/test/kotlin/coverosR3z/server/ServerBDD.kt
+++ src/test/kotlin/coverosR3z/server/ServerBDD.kt
@@ -1,6 +1,14 @@
 package coverosR3z.server
 
+import com.gargoylesoftware.htmlunit.WebClient
+import com.gargoylesoftware.htmlunit.html.DomElement
+import com.gargoylesoftware.htmlunit.html.HtmlPage
+import coverosR3z.main
+import org.junit.Assert.assertEquals
+import org.junit.BeforeClass
+import org.junit.Ignore
 import org.junit.Test
+import org.openqa.selenium.chrome.ChromeDriver
 
 /**
  * As a user of r3z
@@ -9,30 +17,54 @@ import org.junit.Test
  */
 class ServerBDD {
 
+    companion object {
+        @BeforeClass @JvmStatic
+        fun setUp() {
+            // start the server
+            Thread{main()}.start()
+        }
+    }
+
     /*
      *  Here we are testing that a common browser, Chrome, is usable
-     *  for accessing the application.  We will also test on less-common
-     *  but text-oriented browsers, like Lynx, in order to make sure we
+     *  for accessing the application.  We will also test on a simpler
+     *  headless browser without javascript, in order to make sure we
      *  remain "graceful degradation". This also helps keep up mobile-first,
      *  easily accessible, and highly performant.
      */
 
-
     @Test
     fun `happy path - I should be able to see a page - Chrome`() {
         // Given I am a Chrome browser user
+        // Start selenium
+        val driver = ChromeDriver()
+
         // When I go to the homepage
+        driver.get("localhost:8080/sample_template.utl")
+
         // Then I see it successfully in the browser
+        assertEquals("demo", driver.title)
+        driver.quit()
     }
 
     /**
-     * Lynx is a common text-based browser
+     * HtmlUnit is a Java-based headless browser
      */
     @Test
-    fun `happy path - I should be able to see a page - Lynx`() {
-        // Given I am a Lynx browser user
+    fun `happy path - I should be able to see a page without javascript`() {
+        // Given I am on a browser without javascript
+        // start the HtmlUnit headless browser
+        val driver = WebClient()
+        // prevent javascript from running.  We want these tests to really zip.
+        driver.options.isJavaScriptEnabled = false
+
         // When I go to the homepage
+        val page : HtmlPage = driver.getPage("http://localhost:8080/sample_template.utl")
         // Then I see it successfully in the browser
+        val result: DomElement = page.getElementById("username")
+
+        assertEquals("Jona", result.textContent)
+        driver.close()
     }
 
 }
