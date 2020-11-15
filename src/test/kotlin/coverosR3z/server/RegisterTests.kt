b6e6bfc3dbeec30aa07e3d9c8e2a84ffe48a67af
Byron Katz <byron.katz@coveros.com>
2020-11-15 16:38:23 -0500

    Slight maintenance improvement

--- src/test/kotlin/coverosR3z/server/RegisterTests.kt
+++ src/test/kotlin/coverosR3z/server/RegisterTests.kt
@@ -4,8 +4,11 @@ import coverosR3z.DEFAULT_EMPLOYEE
 import coverosR3z.DEFAULT_PASSWORD
 import coverosR3z.DEFAULT_USER
 import coverosR3z.authentication.FakeAuthenticationUtilities
+import coverosR3z.authentication.IAuthenticationUtilities
 import coverosR3z.timerecording.FakeTimeRecordingUtilities
+import coverosR3z.timerecording.ITimeRecordingUtilities
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 /**
@@ -13,23 +16,30 @@ import org.junit.Test
  */
 class RegisterTests {
 
+    lateinit var au : IAuthenticationUtilities
+    lateinit var tru : ITimeRecordingUtilities
+    lateinit var su : ServerUtilities
+
+    @Before
+    fun init() {
+        au = FakeAuthenticationUtilities()
+        tru = FakeTimeRecordingUtilities()
+        su = ServerUtilities(au, tru)
+    }
+
     /**
      * If our code received all the expected
      * values properly, it shouldn't complain
      */
     @Test
     fun testShouldHandleValidInputs() {
-        val au = FakeAuthenticationUtilities()
-        val tru = FakeTimeRecordingUtilities()
         val rd = RequestData(Verb.POST,
                 NamedPaths.REGISTER.path,
-                mapOf(
-                        "username" to DEFAULT_USER.name,
-                        "password" to DEFAULT_PASSWORD,
-                        "employee" to DEFAULT_EMPLOYEE.id.toString()))
-        val responseData = ServerUtilities(au, tru).handleRequestAndRespond(rd)
-        assertTrue(
-                "The system should indicate success.  File was ${responseData.fileContents}",
+                mapOf("username" to DEFAULT_USER.name,
+                      "password" to DEFAULT_PASSWORD,
+                      "employee" to DEFAULT_EMPLOYEE.id.toString()))
+        val responseData = su.handleRequestAndRespond(rd)
+        assertTrue("The system should indicate success.  File was ${responseData.fileContents}",
                 responseData.fileContents.contains("SUCCESS"))
     }
 }
