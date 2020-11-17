7c8392408b713eb80f771b4f217f17ead5039ddd
Byron Katz <byron.katz@coveros.com>
2020-11-16 22:36:11 -0500

    remainder of obvious things to move

---
+++ src/main/kotlin/coverosR3z/authentication/LogoutAPI.kt
@@ -0,0 +1,29 @@
+package coverosR3z.authentication
+
+import coverosR3z.server.*
+
+fun doGETLogout(au: IAuthenticationUtilities, rd: RequestData): PreparedResponseData {
+    return if (ServerUtilities.isAuthenticated(rd)) {
+        au.logout(rd.sessionToken)
+        PreparedResponseData(logoutHTML, ResponseStatus.OK, emptyList())
+    } else {
+        ServerUtilities.redirectTo(NamedPaths.HOMEPAGE.path)
+    }
+}
+
+const val logoutHTML = """
+<html>
+    <head>
+        <title>Logout</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="enter_time.css" />
+    </head>
+    <body>
+            <p>
+                You are now logged out
+            </p>
+
+            <p><a href="homepage">Homepage</a></p>
+    </body>
+</html>    
+"""
