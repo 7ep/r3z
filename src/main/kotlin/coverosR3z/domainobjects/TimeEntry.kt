84a0a7707bcc82f7cdf70f93f20809796319044a
Byron Katz <byron.katz@coveros.com>
2020-11-16 22:19:30 -0500

    Moving some types closer to their parent
    Specifically, Details and Time go in with TimeEntry now

--- src/main/kotlin/coverosR3z/domainobjects/TimeEntry.kt
+++ src/main/kotlin/coverosR3z/domainobjects/TimeEntry.kt
@@ -2,6 +2,27 @@ package coverosR3z.domainobjects
 
 import kotlinx.serialization.Serializable
 
+const val MAX_DETAILS_LENGTH = 500
+
+@Serializable
+data class Details(val value : String = "") {
+    init {
+        require(value.length <= MAX_DETAILS_LENGTH) { "no reason why details for a time entry would ever need to be this big. " +
+                "if you have more to say than the lord's prayer, you're probably doing it wrong." }
+    }
+}
+
+/**
+ * a length of time, in minutes
+ */
+@Serializable
+data class Time(val numberOfMinutes : Int) {
+    init {
+        require(numberOfMinutes > 0) {"Doesn't make sense to have zero or negative time"}
+        require(numberOfMinutes <= 60*24) {"Entries do not span multiple days, thus must be <=24 hrs"}
+    }
+}
+
 /**
  * A data object that stores all the business-related needs for a time entry.
  * For example, if Matt worked for 2 hours on project "A", and had some details
