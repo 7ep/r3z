6859cb8e41f31ea6ab3a86c6c3033a31b6640675
Matthew <matthew.taylor@coveros.com>
2020-11-20 11:14:51 -0500

    Add (broken) css read from file

--- src/main/kotlin/coverosR3z/authentication/RegisterAPI.kt
+++ src/main/kotlin/coverosR3z/authentication/RegisterAPI.kt
@@ -1,6 +1,7 @@
 package coverosR3z.authentication
 
 import coverosR3z.domainobjects.*
+import coverosR3z.misc.FileReader
 import coverosR3z.server.*
 import coverosR3z.timerecording.ITimeRecordingUtilities
 import coverosR3z.webcontent.failureHTML
@@ -68,84 +69,4 @@ fun registerHTML(employees: List<Employee>) : String {
 """
 }
 
-const val registerCSS = """
-    header {
-      color: #ffffff;
-      background-color: #809EE4;
-      font-size: 30px;
-      margin: 0;
-      top: 0;
-      height: 40px;
-      padding: 4px;
-    }
-    
-    body {
-      color: #333333;
-      background-color: #F3F6FA;
-      font-family: "helvetica", sans-serif;
-      padding-top: 40px;
-      margin: 0;
-      padding: 0;
-      display: flex;
-      flex-flow: column wrap;
-    }
-    
-    h2 {
-      text-align: center;
-      font-size: 160%;
-      margin: 10px;
-    }
-    a {
-      color: #ffffff;
-      text-decoration: none;
-    }
-    a:visited {
-      color: #ffffff;
-    }
-    
-    form {
-      font-size: 90%;
-      border: 2px solid #858585;
-      padding: 10px;
-      border-radius: .5em;
-      width: 90%;
-      max-width: 350px;
-      margin: auto;
-    }
-    
-    input {
-      height: 1.7em;
-      margin-bottom: 1em;
-    }
-    
-    button {
-      cursor: pointer;
-      font-size: 120%;;
-      margin: 8px 0 1px 0;
-      padding: 2%;
-      color: #ffff;
-      background-color: #5E64E3;
-      border: none;
-      border-radius: .5em;
-      width: 100%;
-      position: center;
-    }
-    button:hover {
-      color: #5f5f5f;
-      background-color: #DDDDE5;
-    }
-    
-    .container {
-      line-height: 1.5;
-      margin: .2em 0 .2em 0;
-      text-align: left;
-    }
-    
-    .input {
-      border: 1.5px solid #6b6969;
-      border-radius: .3em;
-      width: 100%;
-      background-color: inherit;
-      color: #5f5f5f;
-    }
-"""
+val registerCSS = FileReader.read("general.css")?: ""
