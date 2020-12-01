c14bb9cbb13b574e3892b8d6917af15d0e3a8a02
Byron Katz <byron.katz@coveros.com>
2020-12-01 09:43:19 -0500

    Turn on all logging when running from IDE

--- src/main/kotlin/coverosR3z/Main.kt
+++ src/main/kotlin/coverosR3z/Main.kt
@@ -1,5 +1,7 @@
 package coverosR3z
 
+import coverosR3z.logging.LogTypes
+import coverosR3z.logging.logSettings
 import coverosR3z.logging.logStart
 import coverosR3z.server.Server
 import coverosR3z.server.Server.Companion.extractFirstArgumentAsPort
@@ -8,6 +10,8 @@ import coverosR3z.server.Server.Companion.extractFirstArgumentAsPort
  * Entry point for the application.  KISS.
  */
 fun main(args: Array<String>) {
+    logSettings[LogTypes.DEBUG] = true
+    logSettings[LogTypes.TRACE] = true
     val port = extractFirstArgumentAsPort(args)
     logStart("starting server on port $port")
     val dbDirectory = "db/"
