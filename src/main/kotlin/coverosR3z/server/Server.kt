a78a6ce234ad9eb3e926430c5be5cc46759a0eaa
Mitch Hardee <mitchell.hardee@coveros.com>
2020-12-18 15:52:41 -0500

    WIP: Improving Server Options validation and parsing.
    Co-authored-by: Byron Katz <byron.katz@coveros.com>
    Co-authored-by: Matt Taylor <matthew.taylor@coveros.com>

--- src/main/kotlin/coverosR3z/server/Server.kt
+++ src/main/kotlin/coverosR3z/server/Server.kt
@@ -8,6 +8,7 @@ import coverosR3z.domainobjects.DateTime
 import coverosR3z.domainobjects.SYSTEM_USER
 import coverosR3z.exceptions.ServerOptionsException
 import coverosR3z.logging.*
+import coverosR3z.misc.checkParseToInt
 import coverosR3z.persistence.PureMemoryDatabase
 import coverosR3z.timerecording.ITimeRecordingUtilities
 import coverosR3z.timerecording.TimeEntryPersistence
@@ -65,6 +66,14 @@ class Server(val port: Int, private val dbDirectory: String? = null) {
 
         lateinit var halfOpenServerSocket : ServerSocket
 
+        fun validatePort(arg : String, fullInput : String) {
+            val portNum = checkParseToInt(arg, {"Port number must be an integer value."})
+            if(portNum !in 1..65535) throw ServerOptionsException("port number was out of range.  Range is 1-65535.  Your input was: $fullInput")
+        }
+        fun validateDir(arg : String) : Boolean{
+            return !(arg.startsWith("-p") || arg=="--no-disk-persistence")
+        }
+
         /**
          * Given the command-line arguments, returns the first value
          * as an integer for use as a port number, or defaults to 12345
@@ -76,46 +85,71 @@ class Server(val port: Int, private val dbDirectory: String? = null) {
 //            return if (args.isEmpty() || args[0].isBlank() || portOptionIndex==-1) {
                 ServerOptions()
             } else {
-                var port : Int? = null
-                var db : String? = null
-                var otherArgs: MutableList<String> = mutableListOf()
+                //first boolean in each Pair indicates whether the flag is specified
+                var port = Pair<Boolean, Int?>(false, null)
+                var db = Pair<Boolean, String?>(false, null)
+                var ndp = Pair<Boolean, Boolean?>(false, null)
+                var usedNextI = false
+                //var otherArgs: MutableList<String> = mutableListOf()
                 for (i in args.indices) {
+                    if(usedNextI) {
+                        usedNextI = false
+                        continue
+                    }
 
-                    if (args[i].startsWith("--")) {
-                        if (args[i].length > 2) otherArgs.add(args[i].substring(2))
-
+                    if (args[i]=="--no-disk-persistence") {
+                        if(ndp.first) throw ServerOptionsException("The disk persistence option was specified multiple times. This is not allowed, go to jail.")
+                        ndp = Pair(true, true)
                     } else if (args[i].startsWith("-p")) {
-                        if (args[i].length > 2) port = args[i].substring(2).toIntOrNull()
-                        else if (i+1 < args.size-1) {
-                            port = args[i + 1].toIntOrNull()
-                        }
+                        if(port.first) throw ServerOptionsException("Multiple port values were provided. This is not allowed, go to jail.")
+                        var portStr : String =
+                            when {
+                                args[i].length > 2 -> args[i].substring(2)
+                                i+1 < args.size -> {
+                                    usedNextI = true
+                                    args[i + 1]
+                                }
+                                else -> throw ServerOptionsException("The port option was specified, but no port number was given. This is dumb, you are dumb.")
+                            }
+                        validatePort(portStr, fullInput)
+                        port = Pair(true, portStr.toInt())
                     } else if (args[i].startsWith("-d")) {
-                        if (args[i].length > 2) db = args[i].substring(2)
-                        else if (i+1 < args.size-1){
-                            db = args[i + 1]
-                        }
+                        if(db.first) throw ServerOptionsException("The database option was specified multiple times. This is not allowed, go to jail.")
+                        var dbStr : String =
+                            when {
+                                args[i].length > 2 -> args[i].substring(2)
+                                i+1 < args.size && validateDir(args[i + 1])-> {
+                                    usedNextI = true
+                                    args[i + 1]
+                                }
+                                else -> throw ServerOptionsException("The db option was specified, but no filepath was given. This is dumb, you are dumb.")
+                            }
                     }
                 }
-                if (db == null && otherArgs.isEmpty() && port == null) {
-                    throw ServerOptionsException("The directory option was provided without a directory value")
-                } else if (port != null && port !in 1..65535) {
-                    throw ServerOptionsException("port number was out of range.  Range is 1-65535.  Your input was: $fullInput")
-                } else if (port != null && db != null){
-                    ServerOptions(port, db)
-                } else if (port != null && db == null) {
-                    if (otherArgs.contains("no-disk-persistence")){
-                        ServerOptions(port, dbDirectory=null)
-                    } else {
-                        ServerOptions(port)
-                    }
-                } else if (otherArgs.contains("no-disk-persistence") && db == null) {
-                    ServerOptions(dbDirectory=null)
-                } else if (otherArgs.contains("no-disk-persistence") && db != null) {
-                    throw ServerOptionsException("You cannot combine options to set the database directory with disallowing disk persistence")
-                }
-                else {
-                    ServerOptions(dbDirectory=db)
-                }
+
+                ServerOptions.make(port.second, db.second, ndp.second)
+
+
+//                if (db == null && otherArgs.isEmpty() && port == null) {
+//                    throw ServerOptionsException("The directory option was provided without a directory value")
+//                } else if (port != null && port !in 1..65535) {
+//                    throw ServerOptionsException("port number was out of range.  Range is 1-65535.  Your input was: $fullInput")
+//                } else if (port != null && db != null){
+//                    ServerOptions(port, db)
+//                } else if (port != null && db == null) {
+//                    if (otherArgs.contains("no-disk-persistence")){
+//                        ServerOptions(port, dbDirectory=null)
+//                    } else {
+//                        ServerOptions(port)
+//                    }
+//                } else if (otherArgs.contains("no-disk-persistence") && db == null) {
+//                    ServerOptions(dbDirectory=null)
+//                } else if (otherArgs.contains("no-disk-persistence") && db != null) {
+//                    throw ServerOptionsException("You cannot combine options to set the database directory with disallowing disk persistence")
+//                }
+//                else {
+//                    ServerOptions(dbDirectory=db)
+//                }
 
             }
         }
