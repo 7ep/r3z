e9d86a3f2ead3594d8d5e01898aea5e7aa714b3c
Byron Katz <byron.katz@coveros.com>
2021-01-06 14:21:12 -0500

    Attempting to have cleaner restart

--- docs/operations/restart.sh
+++ docs/operations/restart.sh
@@ -5,12 +5,17 @@ MYDIR="$(dirname "$(readlink -f "$0")")"
 
 # jump to it
 cd $MYDIR
+OLD_PID=$(cat pid)
 
 # stop and run the timekeeping application
-kill $(cat pid); java -jar r3z.jar -p 12345 -d db &>>r3z.log &
+kill $OLD_PID; java -jar r3z.jar -p 12345 -d db &>>r3z.log &
 
 # get the process id, pop it in a file (we'll use this to stop the process later)
 echo $! > pid
+
+# try killing the old process again
+# for some reason it doesn't die right away, sometimes
+kill $OLD_PID
 
 # jump back to where we started
 cd -
