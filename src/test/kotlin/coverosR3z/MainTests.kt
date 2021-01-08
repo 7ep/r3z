8be7bf8fe605fcd429f88eccf06de2be8080b2bc
Byron Katz <byron.katz@coveros.com>
2021-01-07 20:29:25 -0500

    A new idea for testing main
    This way, we examine what is logged, and assert on a known standard.

--- src/test/kotlin/coverosR3z/MainTests.kt
+++ src/test/kotlin/coverosR3z/MainTests.kt
@@ -1,7 +1,8 @@
 package coverosR3z
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.io.File
+import java.io.*
 import kotlin.concurrent.thread
 
 /**
@@ -23,6 +24,8 @@ class MainTests {
     fun testMain() {
         File("db").deleteRecursively()
 
+        val (previousOut, out) = captureSystemOut()
+
         // starts the server
         thread{main(arrayOf("-p","54321"))}
 
@@ -30,6 +33,46 @@ class MainTests {
         // its shutdown hooks, so when we shut it down it will
         // try to do so cleanly
         Thread.sleep(500)
+        System.setOut(previousOut)
+
+        val expectedResults = listOf(
+            "IMPERATIVE: starting server on port 54321",
+            "IMPERATIVE: database directory is db/",
+            "IMPERATIVE: directory db/1/ did not exist.  Returning null for the PureMemoryDatabase",
+            "IMPERATIVE: No existing database found, building new database",
+            "IMPERATIVE: Created new PureMemoryDatabase",
+            "IMPERATIVE: Created the database directory at \"db/1/\"",
+            "IMPERATIVE: Wrote the version of the database (1) to currentVersion.txt")
+
+        compareInOrder(expectedResults, out.toString())
+    }
+
+    /**
+     * So we can capture what the logging would send to the system out, we
+     * will replace the regular out stream with something we control.
+     * We will return both the streams, especially the old one (so we can
+     * set it back the way it was after we're done with it)
+     */
+    private fun captureSystemOut(): Pair<PrintStream, ByteArrayOutputStream> {
+        val previousOut = System.out
+        val out = ByteArrayOutputStream()
+        System.setOut(PrintStream(out))
+        return Pair(previousOut, out)
+    }
+
+    /**
+     * compares a list of strings against a string, to confirm that the list
+     * appears in the string, in order.
+     * @param s one giant block of text
+     * @param stringList a list of strings.  We'll search for each one in the big block, in order.
+     */
+    private fun compareInOrder(stringList : List<String>, s: String) {
+        var startIndex = 0
+        for (string in stringList) {
+            val foundIndex = s.indexOf(string, startIndex)
+            startIndex += foundIndex - startIndex
+            assertTrue("Must find $string in $s", foundIndex > 0)
+        }
     }
 
 }
