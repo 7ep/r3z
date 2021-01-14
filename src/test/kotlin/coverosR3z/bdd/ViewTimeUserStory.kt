228f0e2304c2fcd660258c877712d5dc20316892
Byron Katz <byron.katz@coveros.com>
2021-01-14 15:26:47 -0500

    More conversions to BDD framework

---
+++ src/test/kotlin/coverosR3z/bdd/ViewTimeUserStory.kt
@@ -0,0 +1,11 @@
+package coverosR3z.bdd
+
+object ViewTimeUserStory : UserStory(
+    key = "ViewTime",
+    story =
+    """
+   As an employee
+   I want to see the time entries I have previously entered
+   So that I can confirm my time on work has been accounted correctly
+    """
+)
