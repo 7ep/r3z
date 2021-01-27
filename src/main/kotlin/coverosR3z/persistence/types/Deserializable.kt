1f949ecc053f00652c7f8fab7df14ff0d321c3ac
Byron Katz <byron.katz@coveros.com>
2021-01-27 08:56:20 -0500

    Documentation on deserialization

--- src/main/kotlin/coverosR3z/persistence/types/Deserializable.kt
+++ src/main/kotlin/coverosR3z/persistence/types/Deserializable.kt
@@ -1,7 +1,16 @@
 package coverosR3z.persistence.types
 
+/**
+ * Used for making deserialization generic across
+ * types.  See [deserialize]
+ */
 interface Deserializable<T> {
 
+    /**
+     * Takes a string form of a type and
+     * converts it to its type.
+     * See [coverosR3z.persistence.utility.DatabaseDiskPersistence.deserialize]
+     */
     fun deserialize(str: String) : T
 
 }
