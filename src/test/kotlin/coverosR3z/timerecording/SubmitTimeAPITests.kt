e977fcfe983680c58a1fbff6869974b6bcea2ed6
Byron Katz <byron.katz@coveros.com>
2021-02-11 13:36:31 -0500

    Broken test: API integration test

--- src/test/kotlin/coverosR3z/timerecording/SubmitTimeAPITests.kt
+++ src/test/kotlin/coverosR3z/timerecording/SubmitTimeAPITests.kt
@@ -1,40 +1,60 @@
 package coverosR3z.timerecording
 
-import coverosR3z.authentication.FakeAuthenticationUtilities
+import coverosR3z.authentication.persistence.AuthenticationPersistence
+import coverosR3z.authentication.types.CurrentUser
+import coverosR3z.authentication.utility.AuthenticationUtilities
+import coverosR3z.authentication.utility.IAuthenticationUtilities
+import coverosR3z.misc.DEFAULT_USER
+import coverosR3z.misc.IntegrationTest
+import coverosR3z.misc.types.Date
+import coverosR3z.persistence.utility.PureMemoryDatabase
 import coverosR3z.server.types.*
 import coverosR3z.timerecording.api.SubmitTimeAPI
-import org.junit.Assert
+import coverosR3z.timerecording.persistence.TimeEntryPersistence
+import coverosR3z.timerecording.types.TimePeriod
+import coverosR3z.timerecording.utility.ITimeRecordingUtilities
+import coverosR3z.timerecording.utility.TimeRecordingUtilities
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
 class SubmitTimeAPITests {
 
-    lateinit var au : FakeAuthenticationUtilities
-    lateinit var tru : FakeTimeRecordingUtilities
+    lateinit var au : IAuthenticationUtilities
+    lateinit var tru : ITimeRecordingUtilities
 
     @Before
     fun init() {
-        au = FakeAuthenticationUtilities()
-        tru = FakeTimeRecordingUtilities()
+        val pmd = PureMemoryDatabase()
+        val cu = CurrentUser(DEFAULT_USER)
+        au = AuthenticationUtilities(AuthenticationPersistence(pmd))
+        tru = TimeRecordingUtilities(TimeEntryPersistence(pmd), cu)
     }
 
+    @IntegrationTest
     @Test
     fun testSubmittingTime() {
+        val startDate = "2021-01-01"
+        val endDate = "2021-01-15"
         val data = PostBodyData(mapOf(
-            SubmitTimeAPI.Elements.START_DATE.getElemName() to "2021-01-01",
-            SubmitTimeAPI.Elements.END_DATE.getElemName() to "2021-01-15",
+            SubmitTimeAPI.Elements.START_DATE.getElemName() to startDate,
+            SubmitTimeAPI.Elements.END_DATE.getElemName() to endDate,
         ))
         val sd = ServerData(au, tru, AnalyzedHttpData(data = data), authStatus = AuthStatus.AUTHENTICATED)
+
+        // the API processes the client input
         val response = SubmitTimeAPI.handlePost(sd).statusCode
 
-        Assert.assertEquals(
+        val timePeriod = TimePeriod(Date.make(startDate), Date.make(endDate))
+
+        // get the state from the database so we can confirm the time period was persisted
+        val stp = tru.getSubmittedTimePeriod(timePeriod)
+        assertEquals(stp.bounds, timePeriod)
+
+        // confirm we return the user to the right place
+        assertEquals(
             "We should have gotten redirected to the viewTime page",
             StatusCode.SEE_OTHER, response
         )
-
-        // Validate that it was actually submitted and/or locked date entries
-        // test handlePost()
-        TODO("trigger the handlePost for submit time entry")
-
     }
 }
